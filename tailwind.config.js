ea6eda831acc7e856dde4c507b0f4c55f083b858
Jakob Valentin Seiderer <jakoseid@gmail.com>
2025-06-13 21:14:24 +0000

    new Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 //helps intelligent to work
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        brand: '#ff9900',
-        fancyblue: '#1e90ff',
-      },
-    },
-  },
   content: ["./*.html"],
 };
